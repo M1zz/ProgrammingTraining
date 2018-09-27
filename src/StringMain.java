7b4868f72aad2ea3d6fa34e8a749f680982315af
M1zz <mizzking75@gmail.com>
2018-09-27 23:59:31 +0900

    Initial commit

---
+++ src/StringMain.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringMain {
+    public static void main(String args[]) {
+        // 문제 : 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+        // 입력 : 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+
+        // TODO : 문자열 한개를 입력 받는 다
+        // TODO : 입력받은 문자열을 아스키 코드로 출력한다.
+
+        // 선언문 - 변수의 자료형과 초기값을 선언할 때 사용하는 문장
+
+
+        System.out.print("Type charator ascii : ");
+        Scanner scan = new Scanner(System.in);
+
+        char ascii;
+        ascii = scan.next().charAt(0);
+
+        System.out.print(Integer.valueOf(ascii));
+
+
+    }
+}
