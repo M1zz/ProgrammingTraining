6a8169028684ef40fb8e4ac185b4dd73774d6b9f
M1zz <mizzking75@gmail.com>
2018-09-28 20:35:23 +0900

    클래스 생성 및 기능 분리 / 확장

--- src/StringMain.java
+++ src/StringMain.java
@@ -2,20 +2,12 @@ import java.util.Scanner;
 
 public class StringMain {
     public static void main(String args[]) {
-        // 문제 : 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
-        // 입력 : 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
 
-        // TODO : 문자열 한개를 입력 받는 다
-        // TODO : 입력받은 문자열을 아스키 코드로 출력한다.
-
-        System.out.print("Type charator ascii : ");
-        Scanner scan = new Scanner(System.in);
-
-        char ascii;
-        ascii = scan.next().charAt(0);
-
-        System.out.print(Integer.valueOf(ascii));
-
+        // 문자를 입력받아 아스키를 반환하는 부분
+        /*
+        StringToAscii ascii = new StringToAscii();
+        ascii.asciiConvertor();
+        */
 
     }
 }
