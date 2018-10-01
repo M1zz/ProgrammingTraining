9fe3224cf719db0ec7a1ff07c0a3fa10815ba3c0
M1zz <mizzking75@gmail.com>
2018-10-01 23:21:04 +0900

    테스트 코드 생성

--- src/StringMain.java
+++ src/StringMain.java
@@ -4,10 +4,10 @@ public class StringMain {
     public static void main(String args[]) {
 
         // 문자를 입력받아 아스키를 반환하는 부분
-        /*
+
         StringToAscii ascii = new StringToAscii();
         ascii.asciiConvertor();
-        */
+
 
     }
 }
